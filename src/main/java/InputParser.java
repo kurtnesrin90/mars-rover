4164160c10c56b0ff0af86366fcf7cd8d3a3ce2b
Nesrin Kurt <kurtnesrin90@yahoo.com>
2024-11-14 16:00:48 +0000

    feat: add checkPlateauSize method

--- src/main/java/InputParser.java
+++ src/main/java/InputParser.java
@@ -1,7 +1,39 @@
 
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class InputParser {
 
 
+    public PlateauSize checkPlateauSize(String input) {
+        int first = 0;
+        int second = 0;
+        PlateauSize plateauSize = new PlateauSize(first, second);
+        String[] inputNumbers = input.split(" ");
+        for(int i=0; i<inputNumbers.length; i++) {
+            try {
+                first = Integer.parseInt(inputNumbers[0]);
+            } catch (NumberFormatException e){
+                return null;
+            }
+            try {
+                second = Integer.parseInt(inputNumbers[1]);
+            } catch (NumberFormatException e){
+                return null;
+            }
+            plateauSize = new PlateauSize(first, second);
+        }
+        return plateauSize;
+    }
 
+
+    public void controlDirection(String input) {
+
+    }
 }
+
+
+
+
+
+
