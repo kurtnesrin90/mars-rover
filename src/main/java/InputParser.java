a5e5fe776602048f7ded5cce6c742ca828bd48e2
Nesrin Kurt <kurtnesrin90@yahoo.com>
2024-11-15 12:25:23 +0000

    feat: add parseInstruction() to parse and check if the input is correct

--- src/main/java/InputParser.java
+++ src/main/java/InputParser.java
@@ -9,22 +9,24 @@ public class InputParser {
         int yAxis;
         PlateauSize plateauSize;
 
-        //we can not use empty or null input
+        // we can not use empty or null input
         if(input == null ){
             throw new NullPointerException("can not allowed being null");
         }
         if(input.isEmpty()){
             throw new NullPointerException("Can not allowed being empty");
         }
+        // split the input to string array to check the characters if they are valid or not
         String[] inputNumbers = input.split(" ");
-        xAxis = Integer.parseInt(inputNumbers[0]); //parseInt() can throw an exception
-        yAxis = Integer.parseInt(inputNumbers[1]);
 
-        //we can just have 2 numbers after splitting, otherwise throw an exception
+        xAxis = Integer.parseInt(inputNumbers[0]); //parseInt() can throw an exception
+        yAxis = Integer.parseInt(inputNumbers[1]); //parseInt() can throw an exception
+
+        // we can just have 2 numbers after splitting, otherwise throw an exception
         if (inputNumbers.length != 2) {
             throw new RuntimeException("Invalid size");
         }
-        //we can not use 0 for our plateau
+        // we can not use 0 for our plateau
         if (xAxis == 0 || yAxis == 0){
             throw new IllegalArgumentException("Can not use \"0\"");
                 }
@@ -49,7 +51,7 @@ public class InputParser {
             throw new IllegalArgumentException("Input can not be empty");
         }
 
-        //Split input to the array
+        // split the input to string array to check the characters if they are valid or not
         String[] positionInputValues = input.split(" ");
 
         // we need 3 elements after splitting
@@ -61,6 +63,7 @@ public class InputParser {
         int yAxis = Integer.parseInt(positionInputValues[1]); // parseInt() can throw numbers exception
         CompassDirection facing = CompassDirection.valueOf(positionInputValues[2].toUpperCase());// valueOf() can throw illegal argument exception
 
+        // we can not use negative numbers for position
         if(xAxis < 0 || yAxis < 0){
             throw new RuntimeException("Can not use negative numbers");
         }
@@ -70,32 +73,44 @@ public class InputParser {
     }
 
 
-    public List<String> checkInstruction(String input){
-
+    public List<String> parseInstruction(String input){
+        // create a string list for the instructions
          List<String> result = new ArrayList<>();
          String resultOfL;
          String resultOfR;
          String resultOfM;
-         String[] inputInstructions = input.split("");
-
-         //if input size == 0 , throw an exception
-        if(inputInstructions.length == 0){
-            throw new ArrayIndexOutOfBoundsException("Can not move");
+          // we can not use null input
+        if (input == null){
+            throw new NullPointerException("Can not be null");
         }
 
-        // if input has unwanted value throw an exception
+        //if input size == 0 , throw an exception
+        if(input.isEmpty()){
+            throw new IllegalArgumentException("Can not move");
+        }
+
+         // split the input to check if all the characters is correct
+         String[] inputInstructions = input.split("");
+
+        // if input has an unwanted value throw an exception
          for (int i=0; i<inputInstructions.length; i++){
-             if(!(inputInstructions[i].equals("L") || inputInstructions[i].equals("R") || inputInstructions[i].equals("M"))){
-                 throw new IllegalArgumentException("Instruction is not valid");
+             if(!inputInstructions[i].equals("L") && !inputInstructions[i].equals("R") && !inputInstructions[i].equals("M")){
+                 throw new IllegalArgumentException("Instruction "+ inputInstructions[i] + " is not valid");
              }
+
+             // give instruction for L command
            if(Instruction.valueOf(inputInstructions[i].toUpperCase()) == Instruction.L) {
                resultOfL = "Spins the Rover 90 degrees Left without moving from the current coordinate point";
                result.add(resultOfL);
            }
+
+             // give instruction for R command
            if(Instruction.valueOf(inputInstructions[i].toUpperCase()) == Instruction.R) {
                resultOfR = "Spins the Rover 90 degrees Right without moving from the current coordinate point";
                result.add(resultOfR);
            }
+
+             // give instruction for M command
            if(Instruction.valueOf(inputInstructions[i].toUpperCase()) == Instruction.M) {
                resultOfM = "Moves the Rover forward by one grid point, maintaining the same heading/orientation";
                result.add(resultOfM);
