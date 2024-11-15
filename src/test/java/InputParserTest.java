90a00aa3798c4bbc2238edfe3d06a39d39d127f6
Nesrin Kurt <kurtnesrin90@yahoo.com>
2024-11-15 11:33:09 +0000

    feat: add testParsePositionWithValidInput() to check if the given input is correct

--- src/test/java/InputParserTest.java
+++ src/test/java/InputParserTest.java
@@ -10,23 +10,23 @@ class InputParserTest {
     @DisplayName("Test the given values if they are invalid")
     void testCheckPlateauSizeWithInvalidInput() {
         //Arrange
-        String inputWithNotNumber = "A B";
-        String inputIsNotValidLength = "ABCD";
-        String inputIsEmpty = " ";
-        String inputWithZero = "0 0";
-        String inputWithNumberInvalidLength = "3 4 5";
-        String inputWithNegativeNumber = "-3 5";
-        String inputIsNull = null;
+            String inputWithNotNumber = "A B";
+            String inputIsNotValidLength = "ABCD";
+            String inputIsEmpty = " ";
+            String inputWithZero = "0 0";
+            String inputWithNumberInvalidLength = "3 4 5";
+            String inputWithNegativeNumber = "-3 5";
+            String inputIsNull = null;
         //Act
-        InputParser inputParser = new InputParser();
+            InputParser inputParser = new InputParser();
         //Assert
-        assertThrows(NumberFormatException.class , () -> inputParser.checkPlateauSize(inputWithNotNumber));
-        assertThrows(RuntimeException.class , () -> inputParser.checkPlateauSize(inputIsNotValidLength));
-        assertThrows(RuntimeException.class , () -> inputParser.checkPlateauSize(inputIsEmpty));
-        assertThrows(IllegalArgumentException.class, () -> inputParser.checkPlateauSize(inputWithZero));
-        assertThrows(RuntimeException.class , () -> inputParser.checkPlateauSize(inputWithNumberInvalidLength));
-        assertThrows(RuntimeException.class , () -> inputParser.checkPlateauSize(inputWithNegativeNumber));
-        assertThrows(NullPointerException.class , () -> inputParser.checkPlateauSize(inputIsNull));
+            assertThrows(NumberFormatException.class, () -> inputParser.checkPlateauSize(inputWithNotNumber));
+            assertThrows(RuntimeException.class, () -> inputParser.checkPlateauSize(inputIsNotValidLength));
+            assertThrows(RuntimeException.class, () -> inputParser.checkPlateauSize(inputIsEmpty));
+            assertThrows(IllegalArgumentException.class, () -> inputParser.checkPlateauSize(inputWithZero));
+            assertThrows(RuntimeException.class, () -> inputParser.checkPlateauSize(inputWithNumberInvalidLength));
+            assertThrows(RuntimeException.class, () -> inputParser.checkPlateauSize(inputWithNegativeNumber));
+            assertThrows(NullPointerException.class, () -> inputParser.checkPlateauSize(inputIsNull));
 
     }
 
@@ -34,39 +34,51 @@ class InputParserTest {
     @DisplayName("Test the given values if they are valid")
     void testCheckPlateauSizeWithValidInput() {
         //Arrange
-        String input = "4 5";
+            String input = "4 5";
         //Act
-        InputParser inputParser = new InputParser();
-        PlateauSize plateauSize = inputParser.checkPlateauSize(input);
+            InputParser inputParser = new InputParser();
+            PlateauSize plateauSize = inputParser.checkPlateauSize(input);
         //Assert
-        assertEquals(4, plateauSize.getX());
-        assertEquals(5, plateauSize.getY());
+            assertEquals(4, plateauSize.getX());
+            assertEquals(5, plateauSize.getY());
     }
 
     @Test
     @DisplayName("Test the given inputs for the position, if they are invalid")
-    void testCheckPositionWithInvalidInput() {
-        //Act
-        String inputIsEmpty = "";
-        String inputIsNull = null;
-        String inputWithInvalidLength = "1 2 3 4 N";
-        String inputWithSmallerLength = "1 N";
-        String inputWithoutNumbers = "1 N N";
-        String inputWithAnIllegalDirection = "1 2 A";
-        String inputWithNegativeNumbers = "-4 5 N";
+    void testParsePositionWithInvalidInput() {
         //Arrange
-        InputParser inputParser = new InputParser();
+            String inputIsEmpty = "";
+            String inputIsNull = null;
+            String inputWithInvalidLength = "1 2 3 4 N";
+            String inputWithSmallerLength = "1 N";
+            String inputWithoutNumbers = "1 N N";
+            String inputWithAnIllegalDirection = "1 2 A";
+            String inputWithNegativeNumbers = "-4 5 N";
+        //Act
+            InputParser inputParser = new InputParser();
         //Assert
-        assertThrows(IllegalArgumentException.class , () -> inputParser.checkPosition(inputIsEmpty));
-        assertThrows(NullPointerException.class , () -> inputParser.checkPosition(inputIsNull));
-        assertThrows(RuntimeException.class , () -> inputParser.checkPosition(inputWithInvalidLength));
-        assertThrows(RuntimeException.class , () -> inputParser.checkPosition(inputWithSmallerLength));
-        assertThrows(NumberFormatException.class , () -> inputParser.checkPosition(inputWithoutNumbers));
-        assertThrows(IllegalArgumentException.class , () -> inputParser.checkPosition(inputWithAnIllegalDirection));
-        assertThrows(RuntimeException.class , () -> inputParser.checkPosition(inputWithNegativeNumbers));
+            assertThrows(IllegalArgumentException.class, () -> inputParser.parsePosition(inputIsEmpty));
+            assertThrows(NullPointerException.class, () -> inputParser.parsePosition(inputIsNull));
+            assertThrows(RuntimeException.class, () -> inputParser.parsePosition(inputWithInvalidLength));
+            assertThrows(RuntimeException.class, () -> inputParser.parsePosition(inputWithSmallerLength));
+            assertThrows(NumberFormatException.class, () -> inputParser.parsePosition(inputWithoutNumbers));
+            assertThrows(IllegalArgumentException.class, () -> inputParser.parsePosition(inputWithAnIllegalDirection));
+            assertThrows(RuntimeException.class, () -> inputParser.parsePosition(inputWithNegativeNumbers));
+    }
 
 
-
+        @Test
+        @DisplayName("Test the given inputs for the position, if they are valid")
+        void testParsePositionWithValidInput() {
+        //Arrange
+            String inputIsValid = "6 6 N";
+        //Act
+            InputParser inputParser = new InputParser();
+            Position position = inputParser.parsePosition(inputIsValid);
+        //Assert
+            assertEquals(6 , position.getX());
+            assertEquals(6 , position.getY());
+            assertEquals(CompassDirection.N , position.getFacing());
 
 
 
